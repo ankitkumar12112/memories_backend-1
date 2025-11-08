0ca58178e7bb60c691569a2e2dd5512931f53c71
philosopherakj <138436569+Philosopherakj@users.noreply.github.com>
2025-11-08 13:03:11 +0530

    updated mongo config

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,13 @@
 import { MongoClient } from 'mongodb';
 
-// let password = 'Jatov007'; // Replace with your actual password
-// let encodedPassword = encodeURIComponent(password);
+ //let password = 'Jatov007'; // Replace with your actual password
+ let password = 'Mytimeisnow@12';
+ let encodedPassword = encodeURIComponent(password);
 
 
-// let MONGO_URI = `mongodb+srv://jatovdeepak:${encodedPassword}@cluster0.rrkpuay.mongodb.net/`;
-
-let MONGO_URI = 'mongodb://127.0.0.1/memories';
+ //let MONGO_URI = `mongodb+srv://jatovdeepak:${encodedPassword}@cluster0.rrkpuay.mongodb.net/`;
+ let MONGO_URI =`mongodb+srv://myAtlasDBUser:${encodedPassword}@myatlasclusteredu.8yuh5dc.mongodb.net/?appName=myAtlasClusterEDU`
+//let MONGO_URI = 'mongodb://127.0.0.1/memories';
 
 
 export const client = new MongoClient(MONGO_URI);
